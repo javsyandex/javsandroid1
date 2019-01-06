b89eba0fbf1a9037568b51309d8b2a9ebb89ce17
javsyandex <38355595+javsyandex@users.noreply.github.com>
2019-01-06 18:02:04 +0300

    Add files via upload

---
+++ init.bat
@@ -0,0 +1,6 @@
+echo "# git-repo" >> README.md
+..\git init
+..\git add README.md
+..\git commit -m "first commit"
+..\git remote add origin https://..\github.com/javsyandex/..\git-repo...\git
+..\git push -u origin master
